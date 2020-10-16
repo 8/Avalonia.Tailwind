51704ddb10eef1fac6bfaf6292835c682ca757f4
mdk <mdk@lostindetails.com>
2020-10-16 17:49:39 +0200

    Started with some button examples

---
+++ Examples/Views/Buttons.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Examples.Views
+{
+  public class Buttons : UserControl
+  {
+    public Buttons()
+    {
+      this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+  }
+}
