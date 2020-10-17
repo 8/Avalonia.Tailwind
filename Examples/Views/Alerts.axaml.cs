d70439fe321bb841629a2355031f6d9efb4d2422
mdk <mdk@lostindetails.com>
2020-10-17 22:10:48 +0200

    Added Alerts examples

---
+++ Examples/Views/Alerts.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Examples.Views
+{
+  public class Alerts : UserControl
+  {
+    public Alerts()
+    {
+      this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+  }
+}
